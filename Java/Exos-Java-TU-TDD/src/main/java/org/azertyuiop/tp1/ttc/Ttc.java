af7d50666c40165e48df62d78e9070a3ca2a083c
Margot Laignez <margotlaignez@gmail.com>
2024-01-02 16:53:34 +0100

    :sparkles: Ajout des classes à tester
    Copier-coller de la correction des exercices moyenne, prix TTC, nombre palindrome, doublon, nombre majoritaire, nombre romain vers nombre arabe,

--- Java/Exos-Java-TU-TDD/src/main/java/org/azertyuiop/tp1/ttc/Ttc.java
+++ Java/Exos-Java-TU-TDD/src/main/java/org/azertyuiop/tp1/ttc/Ttc.java
@@ -1,4 +1,40 @@
 package org.azertyuiop.tp1.ttc;
 
 public class Ttc {
+
+    /**
+     * Taux de TVA à 20%
+     */
+    public final double TVA = 1.20;
+
+    /**
+     * Calcule le prix TTC à partir du prix HT
+     * @param prixHt Prix HT
+     * @return Prix TTC
+     */
+    public double HtToTtc(int prixHt) {
+
+        verifierPrix(prixHt);
+
+        return ajouterTva(prixHt);
+    }
+
+    /**
+     * Ajoute la TVA à un nombre
+     * @param prixHt Nombre à multiplier par la TVA
+     * @return Nombre multiplié par la TVA
+     */
+    private double ajouterTva(int prixHt) {
+        return prixHt * this.TVA;
+    }
+
+    /**
+     * Vérifie que le prix HT est positif
+     * @param prixHt Prix HT
+     */
+    private void verifierPrix(int prixHt) {
+        if (prixHt < 0) {
+            throw new IllegalArgumentException("Le prix HT doit être positif");
+        }
+    }
 }
