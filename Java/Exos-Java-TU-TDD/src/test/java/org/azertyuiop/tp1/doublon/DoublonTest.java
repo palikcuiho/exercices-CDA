7d57ef7d0b176e5872da346cdbfacfeba42d6817
Margot Laignez <margotlaignez@gmail.com>
2024-01-03 14:08:49 +0100

    :white_check_mark: Ajout des tests pour "Exercice 4 : Contient un doublon ?"

--- Java/Exos-Java-TU-TDD/src/test/java/org/azertyuiop/tp1/doublon/DoublonTest.java
+++ Java/Exos-Java-TU-TDD/src/test/java/org/azertyuiop/tp1/doublon/DoublonTest.java
@@ -1,6 +1,44 @@
 package org.azertyuiop.tp1.doublon;
 
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class DoublonTest {
+
+    private Doublon doublon;
+
+    @BeforeEach
+    public void setUp() {
+        doublon = new Doublon();
+    }
+
+    @DisplayName("Test si le tableau contient un doublon")
+    @ParameterizedTest
+    @CsvSource({"1, 1, 2"})
+    public void testDoublon(int premierNombre, int deuxiemeNombre, int troisiemeNombre){
+        int[] tableauATester = new int[]{premierNombre, deuxiemeNombre, troisiemeNombre};
+        boolean resultat = doublon.verifierSiDoublons(tableauATester);
+        assertTrue(resultat);
+    };
+
+    @DisplayName("Test si le tableau ne contient pas de doublon")
+    @ParameterizedTest
+    @CsvSource({"1, 2, 3"})
+    public void testPasDeDoublon(int premierNombre, int deuxiemeNombre, int troisiemeNombre){
+        int[] tableauATester = new int[]{premierNombre, deuxiemeNombre, troisiemeNombre};
+        boolean resultat = doublon.verifierSiDoublons(tableauATester);
+        assertFalse(resultat);
+    }
+
 
 }
