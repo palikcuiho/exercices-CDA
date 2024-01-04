1cf876adb09f8d3b61a2122f6ec9d6e77570b554
Margot Laignez <margotlaignez@gmail.com>
2024-01-04 13:28:51 +0100

    :sparkles: Création de la classe Salarie

---
+++ Java/TU-TP2/src/main/java/org/azertyuiop/tp2/Salarie.java
@@ -0,0 +1,35 @@
+package org.azertyuiop.tp2;
+
+public abstract class Salarie {
+    public Salaire salaire;
+
+    public Salarie(Salaire salaire, Identite identite) {
+        this.salaire = salaire;
+        this.identite = identite;
+    }
+
+    public Identite identite;
+    public static final double TEMPS_PLEIN = 151.67;
+
+    public void travailler(){
+        travailler(TEMPS_PLEIN);
+    }
+    public void travailler(double nombreHeures){
+        String nomSalarie = this.identite.getNomComplet();
+        double salaire = this.salaire.payer(nombreHeures);
+        System.out.println("Le salarié " + this.identite.getNomComplet()
+                + " a travaillé "
+                + nombreHeures
+                + " heures et a perçu "
+                + salaire
+                + " euros.");
+    }
+
+    public void demenager(Adresse nouvelleAdresse){
+        this.identite.adresse = nouvelleAdresse;
+    }
+
+    public void augmenter(float pourcentage){
+        this.salaire.tauxHoraire = this.salaire.tauxHoraire*pourcentage/100;
+    }
+}
