e85667d6587d2b739a8547882059ef751d9fadc5
Margot Laignez <margotlaignez@gmail.com>
2024-01-04 11:41:18 +0100

    :sparkles: Creéation de la classe Adresse

---
+++ Java/TU-TP2/src/main/java/org/azertyuiop/tp2/Adresse.java
@@ -0,0 +1,18 @@
+package org.azertyuiop.tp2;
+
+public class Adresse {
+
+    public String numero;
+    public String rue;
+    public String codePostal;
+    public String ville;
+    public String pays;
+
+    public Adresse(String numero, String rue, String codePostal, String ville, String pays) {
+        this.numero = numero;
+        this.rue = rue;
+        this.codePostal = codePostal;
+        this.ville = ville;
+        this.pays = pays;
+    }
+}
