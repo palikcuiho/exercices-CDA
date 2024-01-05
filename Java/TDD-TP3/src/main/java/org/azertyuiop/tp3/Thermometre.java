d35a53194690e9462ba2f8498c4fe18958bbf9c0
Margot Laignez <margotlaignez@gmail.com>
2024-01-05 15:38:50 +0100

    test : throws when array has more than 10000 values :white_check_mark:

--- Java/TDD-TP3/src/main/java/org/azertyuiop/tp3/Thermometre.java
+++ Java/TDD-TP3/src/main/java/org/azertyuiop/tp3/Thermometre.java
@@ -3,6 +3,8 @@ package org.azertyuiop.tp3;
 public class Thermometre {
 
     public int getTemperatureClosestToZero(int[] temperatures) {
+        if (temperatures.length > 10000)
+            throw new IllegalArgumentException("Le nombre de mesures ne doit pas excéder 10000");
         return 0;
     }
 }
