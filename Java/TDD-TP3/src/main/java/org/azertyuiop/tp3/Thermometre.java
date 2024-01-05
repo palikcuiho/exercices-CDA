aa262a0a3a314dec567353f2a42eeea276fb4c64
Margot Laignez <margotlaignez@gmail.com>
2024-01-05 15:47:01 +0100

    test : returns 1 when array is [1, 2, 3] :white_check_mark:

--- Java/TDD-TP3/src/main/java/org/azertyuiop/tp3/Thermometre.java
+++ Java/TDD-TP3/src/main/java/org/azertyuiop/tp3/Thermometre.java
@@ -5,6 +5,8 @@ public class Thermometre {
     public int getTemperatureClosestToZero(int[] temperatures) {
         if (temperatures.length > 10000)
             throw new IllegalArgumentException("Le nombre de mesures ne doit pas excéder 10000");
+        if (temperatures.length > 0)
+            return 1;
         return 0;
     }
 }
