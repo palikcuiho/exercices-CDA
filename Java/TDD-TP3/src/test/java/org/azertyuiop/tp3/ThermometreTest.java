b366728d2897f0736ceb31e0db5cda7b6ccba20b
Margot Laignez <margotlaignez@gmail.com>
2024-01-05 15:34:53 +0100

    test : should throw when array has more than 10000 values :test_tube:

--- Java/TDD-TP3/src/test/java/org/azertyuiop/tp3/ThermometreTest.java
+++ Java/TDD-TP3/src/test/java/org/azertyuiop/tp3/ThermometreTest.java
@@ -1,16 +1,32 @@
 package org.azertyuiop.tp3;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ThermometreTest {
 
+    private Thermometre thermometre;
+
+    @BeforeEach
+    public void setup(){thermometre = new Thermometre();}
+
     @Test
     public void should_return_0_when_array_is_empty(){
-        Thermometre thermometre = new Thermometre();
         int[] temperatures = {};
         int result = thermometre.getTemperatureClosestToZero(temperatures);
         assertEquals(0, result);
     }
+
+    @Test
+    public void should_throw_if_array_is_too_big(){
+        int[] temperatures = new int[10001];
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> thermometre.getTemperatureClosestToZero(temperatures));
+    }
+
+
 }
