da66b779090ae756e063910d68c1420e7e918ab9
Margot Laignez <margotlaignez@gmail.com>
2024-01-05 16:24:17 +0100

    test : returns the smallest value in an array of positive values :white_check_mark:

--- Java/TDD-TP3/src/main/java/org/azertyuiop/tp3/Thermometre.java
+++ Java/TDD-TP3/src/main/java/org/azertyuiop/tp3/Thermometre.java
@@ -3,10 +3,16 @@ package org.azertyuiop.tp3;
 public class Thermometre {
 
     public int getTemperatureClosestToZero(int[] temperatures) {
+        if (temperatures.length == 0)
+            return 0;
         if (temperatures.length > 10000)
             throw new IllegalArgumentException("Le nombre de mesures ne doit pas excéder 10000");
-        if (temperatures.length > 0)
-            return 1;
-        return 0;
+        int currentSmallestValue = 0;
+        for(int index = 0;index < temperatures.length;index++) {
+            if (temperatures[index] < currentSmallestValue || index == 0 ){
+                currentSmallestValue = temperatures[index];
+            }
+        }
+        return currentSmallestValue;
     }
 }
